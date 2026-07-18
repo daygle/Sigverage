9de5b0655ce0493f34e0bfd287f5f12bebc9508d
daygle <daygle@daygle.net>
2026-07-18 12:10:17 +1000

    Resolve build warning: Unable to strip libandroidx.graphics.path.so

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,6 +58,9 @@ android {
     }
 
     packaging {
+        jniLibs {
+            keepDebugSymbols += "**/libandroidx.graphics.path.so"
+        }
         resources {
             excludes += setOf(
                 "/META-INF/{AL2.0,LGPL2.1}",
